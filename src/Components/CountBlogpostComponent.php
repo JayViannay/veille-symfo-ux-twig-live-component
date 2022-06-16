d5c86360524d1eddbf875bff5b2ebb7a5fa243f1
JennyViannay <jenny.viannay75@gmail.com>
2022-06-16 23:12:47 +0200

    add cherry

---
+++ src/Components/CountBlogpostComponent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Components;
+
+use App\Repository\BlogRepository;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent('count_blogpost')]
+class CountBlogpostComponent 
+{
+    use DefaultActionTrait;
+
+    public function __construct(private BlogRepository $blogRepository) {}
+
+    public function countBlogpost(): int
+    {
+        return $this->blogRepository->count([]);
+    }
+}
